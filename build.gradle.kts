299133897e9a78a2c247df9b0fa2645c2b74a7ad
kain <zhangtqx@vip.qq.com>
2019-12-07 13:55:46 +0800

    Submit Datasource

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,11 +18,12 @@ repositories {
 
 dependencies {
     implementation("com.alibaba:fastjson:1.2.61")
-    //    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.mariadb.jdbc:mariadb-java-client")
     testImplementation("org.springframework.boot:spring-boot-starter-test") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
