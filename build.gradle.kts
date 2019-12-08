b75b5b2dd4436f0f94a0dfe5707889f77ea9ddc4
kain <kainpro@foxmail.com>
2019-12-08 21:13:23 +0800

    Test Entity

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,9 +24,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.mariadb.jdbc:mariadb-java-client")
-//    testImplementation("org.springframework.boot:spring-boot-starter-test") {
-//        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
-//    }
+    testImplementation("org.springframework.boot:spring-boot-starter-test") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
 }
 
 tasks.withType<Test> {
--- src/main/kotlin/van/api/entities/Resource.kt
+++ src/main/kotlin/van/api/entities/Resource.kt
@@ -1,11 +1,12 @@
 package van.api.entities
 
+import org.hibernate.annotations.ColumnDefault
 import org.hibernate.type.BooleanType
 import org.hibernate.type.TextType
 import javax.persistence.*
 
-@Entity(name = "v_acl")
-class Acl {
+@Entity(name = "v_resource")
+class Resource {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(columnDefinition = "int(10) unsigned")
@@ -14,14 +15,23 @@ class Acl {
     @Column(name = "`key`", unique = true, nullable = false, length = 40)
     var key: String? = null
 
+    @Column(nullable = false, columnDefinition = "varchar(40) default 'origin'")
+    var parent: String? = null
+
     @Column(nullable = false)
     var name: TextType? = null
 
-    @Column(name = "`write`", nullable = false)
-    var write: TextType? = null
+    @Column(nullable = false, columnDefinition = "bit default 0")
+    var nav: BooleanType? = null
 
-    @Column(name = "`read`", nullable = false)
-    var read: TextType? = null
+    @Column(nullable = false, columnDefinition = "bit default 0")
+    var router: BooleanType? = null
+
+    @Column(nullable = false, columnDefinition = "bit default 0")
+    var policy: BooleanType? = null
+
+    @Column(nullable = true, length = 50)
+    var icon: String? = null
 
     @Column(nullable = false, columnDefinition = "bit default 1")
     var status: BooleanType? = null
