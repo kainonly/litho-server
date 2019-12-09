b014cff394195e457fe47f4cd5918383ede8221e
kain <kainpro@foxmail.com>
2019-12-09 21:00:01 +0800

    Test Entity

--- src/main/kotlin/van/api/entities/AdminBasic.kt
+++ src/main/kotlin/van/api/entities/AdminBasic.kt
@@ -1,5 +1,6 @@
 package van.api.entities
 
+import org.hibernate.type.TextType
 import javax.persistence.*
 
 @Entity(name = "v_admin_basic")
@@ -9,5 +10,17 @@ class AdminBasic {
     @Column(columnDefinition = "int(10) unsigned")
     var id: Int? = 0
 
+    @Column(nullable = false, length = 255)
+    var password: TextType? = null
 
+    @Column(length = 100)
+    var email: String? = null
+
+    @Column(length = 20)
+    var phone: String? = null
+
+    @Column(length = 30)
+    var call: String? = null
+
+    var avatar: String? = null
 }
