517ab72e5903411bad9f90978d0a622ea78ce135
kain <zhangtqx@vip.qq.com>
2020-03-22 14:00:35 +0800

    更新微服务单元测试

--- test/Microservice/SSHServiceTest.php
+++ test/Microservice/SSHServiceTest.php
@@ -19,6 +19,7 @@ use SSHMicroservice\NoParameter;
 use SSHMicroservice\PutParameter;
 use SSHMicroservice\Response;
 use SSHMicroservice\RouterClient;
+use SSHMicroservice\TestingParameter;
 use SSHMicroservice\TunnelOption;
 use SSHMicroservice\TunnelsParameter;
 
@@ -39,9 +40,27 @@ class SSHServiceTest extends TestCase
             $this->config = yaml_parse($context);
             $key = $filesystem->get('../config/key-1.pem');
             $this->config[0]['private_key'] = base64_encode($key);
+            $key = $filesystem->get('../config/key-2.pem');
+            $this->config[1]['private_key'] = base64_encode($key);
         } catch (Exception $e) {
             $this->expectException($e->getMessage());
         }
+    }
+
+    public function testConnect()
+    {
+        $param = new TestingParameter();
+        $param->setHost($this->config[0]['host']);
+        $param->setPort($this->config[0]['port']);
+        $param->setUsername($this->config[0]['username']);
+        $param->setPassword($this->config[0]['password'] ?? '');
+        $param->setPrivateKey($this->config[0]['private_key']);
+        $param->setPassphrase($this->config[0]['passphrase'] ?? '');
+        /**
+         * @var Response $response
+         */
+        list($response, $status) = $this->client->Testing($param)->wait();
+        $this->assertEquals($response->getError(), 0, $response->getMsg());
     }
 
     public function testPut()
@@ -74,6 +93,36 @@ class SSHServiceTest extends TestCase
         var_dump($response->getData());
     }
 
+    public function testPutOther()
+    {
+        $param = new PutParameter();
+        $param->setIdentity('other');
+        $param->setHost($this->config[1]['host']);
+        $param->setPort($this->config[1]['port']);
+        $param->setUsername($this->config[1]['username']);
+        $param->setPassword($this->config[1]['password'] ?? '');
+        $param->setPrivateKey($this->config[1]['private_key']);
+        $param->setPassphrase($this->config[1]['passphrase'] ?? '');
+        /**
+         * @var Response $response
+         */
+        list($response, $status) = $this->client->Put($param)->wait();
+        $this->assertEquals($response->getError(), 0, $response->getMsg());
+    }
+
+    public function testExecOther()
+    {
+        $param = new ExecParameter();
+        $param->setIdentity('other');
+        $param->setBash('uptime');
+        /**
+         * @var ExecResponse $response
+         */
+        list($response, $status) = $this->client->Exec($param)->wait();
+        $this->assertEquals($response->getError(), 0, $response->getMsg());
+        var_dump($response->getData());
+    }
+
     public function testGet()
     {
         $param = new GetParameter();
@@ -86,10 +135,22 @@ class SSHServiceTest extends TestCase
         var_dump($response->getData()->serializeToJsonString());
     }
 
+    public function testGetOther()
+    {
+        $param = new GetParameter();
+        $param->setIdentity('other');
+        /**
+         * @var GetResponse $response
+         */
+        list($response, $status) = $this->client->Get($param)->wait();
+        $this->assertEquals($response->getError(), 0, $response->getMsg());
+        var_dump($response->getData()->serializeToJsonString());
+    }
+
     public function testLists()
     {
         $param = new ListsParameter();
-        $param->setIdentity(['test']);
+        $param->setIdentity(['test', 'other']);
         /**
          * @var ListsResponse $response
          */
@@ -143,10 +204,35 @@ class SSHServiceTest extends TestCase
         $this->assertEquals($response->getError(), 0, $response->getMsg());
     }
 
+    public function testTunnelsOther()
+    {
+        $param = new TunnelsParameter();
+        $param->setIdentity('other');
+        $param->setTunnels([
+            $this->setTunnelOption('127.0.0.1', 80, '127.0.0.1', 8080),
+        ]);
+        /**
+         * @var Response $response
+         */
+        list($response, $status) = $this->client->Tunnels($param)->wait();
+        $this->assertEquals($response->getError(), 0, $response->getMsg());
+    }
+
     public function testDelete()
     {
         $param = new DeleteParameter();
         $param->setIdentity('test');
+        /**
+         * @var Response $response
+         */
+        list($response, $status) = $this->client->Delete($param)->wait();
+        $this->assertEquals($response->getError(), 0, $response->getMsg());
+    }
+
+    public function testDeleteOther()
+    {
+        $param = new DeleteParameter();
+        $param->setIdentity('other');
         /**
          * @var Response $response
          */
