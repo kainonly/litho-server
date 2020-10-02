17b294df6fe13d4329febc28ceba9263707b5484
kain <kainpro@foxmail.com>
2020-10-02 14:15:58 +0800

    update redis

--- app/types/config.go
+++ app/types/config.go
@@ -4,4 +4,5 @@ type Config struct {
 	Listen string      `yaml:"listen"`
 	Cors   CorsOption  `yaml:"cors"`
 	Mysql  MysqlOption `yaml:"mysql"`
+	Redis  RedisOption `yaml:"redis"`
 }
---
+++ app/types/redis-option.go
@@ -0,0 +1,7 @@
+package types
+
+type RedisOption struct {
+	Address  string `yaml:"address"`
+	Password string `yaml:"password"`
+	DB       int    `yaml:"db"`
+}
