c313c69a4d53db11151069557b28ccd45380edf4
kain <kainpro@foxmail.com>
2020-10-02 13:58:03 +0800

    update gorm

--- app/app.go
+++ app/app.go
@@ -3,6 +3,10 @@ package app
 import (
 	"github.com/kataras/iris/v12"
 	"github.com/kataras/iris/v12/context"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"log"
+	"time"
 	"van-api/app/controller"
 	"van-api/app/middleware/cors"
 	"van-api/app/types"
@@ -13,6 +17,23 @@ func Application(option *types.Config) {
 	app.Use(cors.Cors(types.CorsOption{}))
 	context.DefaultJSONOptions = context.JSON{
 		StreamingJSON: true,
+	}
+	db, err := gorm.Open(mysql.Open(option.Mysql.Dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	sqlDB, err := db.DB()
+	if err != nil {
+		log.Fatalln(err)
+	}
+	if option.Mysql.MaxIdleConns != 0 {
+		sqlDB.SetMaxIdleConns(option.Mysql.MaxIdleConns)
+	}
+	if option.Mysql.MaxOpenConns != 0 {
+		sqlDB.SetMaxOpenConns(option.Mysql.MaxOpenConns)
+	}
+	if option.Mysql.ConnMaxLifetime != 0 {
+		sqlDB.SetConnMaxLifetime(time.Second * time.Duration(option.Mysql.ConnMaxLifetime))
 	}
 	control := controller.New()
 	app.Get("/", control.Default)
---
+++ app/types/mysql-option.go
@@ -0,0 +1,8 @@
+package types
+
+type MysqlOption struct {
+	Dsn             string `yaml:"dsn"`
+	MaxIdleConns    int    `yaml:"max_idle_conns"`
+	MaxOpenConns    int    `yaml:"max_open_conns"`
+	ConnMaxLifetime int    `yaml:"conn_max_lifetime"`
+}
