1139a90720245113b0708fb706c3055626fafc1b
kain <zhangtqx@vip.qq.com>
2020-10-14 17:58:59 +0800

    update utils

--- app/utils/hash/hash_test.go
+++ app/utils/hash/hash_test.go
@@ -1,6 +1,11 @@
 package hash
 
-import "testing"
+import (
+	"strconv"
+	"testing"
+)
+
+var checkHash string
 
 func TestMake(t *testing.T) {
 	hash, err := Make(`pass`, Option{})
@@ -8,12 +13,35 @@ func TestMake(t *testing.T) {
 		t.Error(err)
 	}
 	t.Log(hash)
+	checkHash = hash
 }
 
 func TestCheck(t *testing.T) {
-	result := Check(
+	result, err := Check(
 		`pass`,
-		`$argon2id$v=19$m=65536,t=4,p=1$HDPsadBAbRx8MV9opmyZ2A$UkhI1agUWgpGCbcofy6n17xdoSLIh0wu9HOXRZMWkhE`,
+		checkHash,
 	)
+	if err != nil {
+		t.Error(err)
+	}
 	t.Log(result)
+}
+
+func BenchmarkMakeAndCheck(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		pass := "pass" + strconv.Itoa(i)
+		hash, err := Make(pass, Option{})
+		if err != nil {
+			b.Error(err)
+		}
+		result, err := Check(pass, hash)
+		if err != nil {
+			b.Error(err)
+		}
+		if result == false {
+			b.Error("false")
+		}
+		b.Log(pass, ":", result)
+	}
+
 }
