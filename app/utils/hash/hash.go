4ac38418e2a021704133430acbe53b3d475109b9
kain <kainpro@foxmail.com>
2020-10-14 17:58:59 +0800

    update utils

--- app/utils/hash/hash.go
+++ app/utils/hash/hash.go
@@ -2,7 +2,9 @@ package hash
 
 import (
 	"crypto/rand"
+	"crypto/subtle"
 	"encoding/base64"
+	"errors"
 	"golang.org/x/crypto/argon2"
 	"regexp"
 	"strconv"
@@ -15,13 +17,18 @@ var (
 	DefaultThreads = uint8(1)
 )
 
+var (
+	ErrInvalidHash         = errors.New(`the encoded hash is not in the correct format`)
+	ErrIncompatibleVersion = errors.New(`incompatible version of argon2`)
+)
+
 type Option struct {
 	Time    uint32
 	Memory  uint32
 	Threads uint8
 }
 
-func Make(password string, option Option) (hash string, err error) {
+func Make(password string, option Option) (hashedPassword string, err error) {
 	salt := make([]byte, 16)
 	_, err = rand.Read(salt)
 	if err != nil {
@@ -36,22 +43,63 @@ func Make(password string, option Option) (hash string, err error) {
 	if option.Threads == 0 {
 		option.Threads = DefaultThreads
 	}
-	key := argon2.IDKey([]byte(password), salt, option.Time, option.Memory, option.Threads, 32)
+	hash := argon2.IDKey([]byte(password), salt, option.Time, option.Memory, option.Threads, 32)
 	var build strings.Builder
 	build.WriteString("$argon2id$v=" + strconv.Itoa(argon2.Version))
 	build.WriteString("$m=" + strconv.Itoa(int(option.Memory)))
 	build.WriteString(",t=" + strconv.Itoa(int(option.Time)))
 	build.WriteString(",p=" + strconv.Itoa(int(option.Threads)))
 	build.WriteString("$" + base64.RawStdEncoding.EncodeToString(salt))
-	build.WriteString("$" + base64.RawStdEncoding.EncodeToString(key))
-	hash = build.String()
+	build.WriteString("$" + base64.RawStdEncoding.EncodeToString(hash))
+	hashedPassword = build.String()
 	return
 }
 
-func Check(password string, hash string) bool {
-	println(password)
-	println(hash)
-	r := regexp.MustCompile(hash)
-	r.FindString(`$`)
-	return true
+func Check(password string, hashedPassword string) (result bool, err error) {
+	args := regexp.
+		MustCompile(`^\$(\w+)\$v=(\d+)\$m=(\d+),t=(\d+),p=(\d+)\$(\S+)\$(\S+)`).
+		FindStringSubmatch(hashedPassword)
+	if len(args) != 8 {
+		return false, ErrInvalidHash
+	}
+	if args[1] != `argon2id` {
+		return false, ErrInvalidHash
+	}
+	version, err := strconv.Atoi(args[2])
+	if err != nil {
+		return false, err
+	}
+	if version != argon2.Version {
+		return false, ErrIncompatibleVersion
+	}
+	memory, err := strconv.ParseUint(args[3], 10, 32)
+	if err != nil {
+		return false, err
+	}
+	time, err := strconv.ParseUint(args[4], 10, 32)
+	if err != nil {
+		return false, err
+	}
+	threads, err := strconv.Atoi(args[5])
+	if err != nil {
+		return false, err
+	}
+	option := Option{
+		Memory:  uint32(memory),
+		Time:    uint32(time),
+		Threads: uint8(threads),
+	}
+	decodeSalt, err := base64.RawStdEncoding.DecodeString(args[6])
+	if err != nil {
+		return false, err
+	}
+	hash, err := base64.RawStdEncoding.DecodeString(args[7])
+	if err != nil {
+		return false, err
+	}
+	newHash := argon2.IDKey([]byte(password), decodeSalt, option.Time, option.Memory, option.Threads, 32)
+	if subtle.ConstantTimeCompare(hash, newHash) == 1 {
+		return true, nil
+	}
+	return false, nil
 }
--- go.mod
+++ go.mod
@@ -6,7 +6,8 @@ require (
 	github.com/go-redis/redis/v8 v8.2.3
 	github.com/json-iterator/go v1.1.10
 	github.com/kataras/iris/v12 v12.2.0-alpha
-	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a
+	golang.org/x/crypto v0.0.0-20201012173705-84dcc777aaee
+	golang.org/x/sys v0.0.0-20201014080544-cc95f250f6bc // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200615113413-eeeca48fe776
 	gorm.io/driver/mysql v1.0.2
 	gorm.io/gorm v1.20.2
