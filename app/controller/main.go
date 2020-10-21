9546c878cd319b0069708605728558dd378ca15e
kain <zhangtqx@vip.qq.com>
2020-10-22 00:10:35 +0800

    update

--- app/controller/main.go
+++ app/controller/main.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"github.com/kataras/iris/v12"
+	"log"
+	"van-api/app/cache"
 	"van-api/helper/res"
 	"van-api/helper/validate"
 )
@@ -14,12 +16,7 @@ type LoginBody struct {
 	Password string `validate:"required,min=12,max=20"`
 }
 
-func (c *MainController) PostLogin(ctx iris.Context) interface{} {
-	var body LoginBody
-	err := ctx.ReadJSON(&body)
-	if err != nil {
-		return res.Error(err)
-	}
+func (c *MainController) PostLogin(body LoginBody, cache *cache.Model) interface{} {
 	errs := validate.Make(body, validate.Message{
 		"Username": map[string]string{
 			"required": "Submit missing [username] field",
@@ -28,6 +25,11 @@ func (c *MainController) PostLogin(ctx iris.Context) interface{} {
 	if errs != nil {
 		return res.Error(errs)
 	}
+	result, err := cache.AdminGet(body.Username)
+	if err != nil {
+		return res.Error(err)
+	}
+	log.Println(result)
 	return res.Ok()
 }
 
--- curd/curd.go
+++ curd/curd.go
@@ -1,6 +1,10 @@
 package curd
 
-import "gorm.io/gorm"
+import (
+	"gorm.io/gorm"
+	"reflect"
+	"van-api/helper/res"
+)
 
 type Curd struct {
 	db *gorm.DB
@@ -10,4 +14,11 @@ func Initialize(db *gorm.DB) *Curd {
 	c := new(Curd)
 	c.db = db
 	return c
+}
+
+func (c *Curd) Originlists(model interface{}) interface{} {
+	slice := reflect.MakeSlice(reflect.SliceOf(reflect.TypeOf(model)), 0, 0)
+	lists := reflect.New(slice.Type())
+	c.db.Find(lists.Interface())
+	return res.Data(lists.Interface())
 }
