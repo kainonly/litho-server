41381f3a52385164d5da4ede27ea47f3e5a4c234
kain <zhangtqx@vip.qq.com>
2020-11-23 00:58:07 +0800

    update migrate

--- database/role_policy/setup.go
+++ database/role_policy/setup.go
@@ -8,10 +8,10 @@ import (
 func Setup(db *gorm.DB, cfg *config.Config) error {
 	opt := cfg.Database
 	sql := "create view ? as "
-	sql += "select r.role_key, p.acl_key, max(p.policy) as policy "
-	sql += "from ? r "
-	sql += "join ? p on r.resource_key = p.resource_key "
-	sql += "group by r.role_key, p.acl_key"
+	sql += "select rrr.role_key, p.acl_key, max(p.policy) as policy "
+	sql += "from ? rrr "
+	sql += "join ? p on rrrresource_key = p.resource_key "
+	sql += "group by rrr.role_key, p.acl_key"
 	return db.Exec(sql,
 		opt.Table("role_policy"),
 		opt.Table("role_resource_rel"),
