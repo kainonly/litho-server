1462b78f8f60070d5d003d1118779a433a6fdfc5
kain <kainpro@foxmail.com>
2021-02-12 14:42:48 +0800

    update

--- application/controller/main.go
+++ application/controller/main.go
@@ -1,11 +1,13 @@
 package controller
 
 import (
+	"context"
 	"errors"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/gin-gonic/gin"
 	"github.com/kainonly/gin-extra/authx"
 	"github.com/kainonly/gin-extra/hash"
+	"github.com/kainonly/gin-extra/mvcx"
 	"github.com/kainonly/gin-extra/typ"
 	"lab-api/application/common"
 	"net/http"
@@ -29,9 +31,19 @@ func (c *Controller) Login(ctx *gin.Context) interface{} {
 	if admin == nil {
 		return errors.New("user does not exist or has been frozen")
 	}
+	cb := context.Background()
+	if !c.Redis.UserLock.Check(cb, "admin:"+body.Username) {
+		c.Redis.UserLock.Lock(cb, "admin:"+body.Username)
+		return mvcx.Response{
+			Code: 2,
+			Msg:  "you have failed to log in too many times, please try again later",
+		}
+	}
 	if ok, _ := hash.Verify(body.Password, admin["password"].(string)); !ok {
+		c.Redis.UserLock.Inc(cb, "admin:"+body.Username)
 		return errors.New("user login password is incorrect")
 	}
+	c.Redis.UserLock.Remove("admin:" + body.Username)
 	if err := authx.Create(ctx, typ.Cookie{
 		Name:     "system",
 		Secure:   true,
