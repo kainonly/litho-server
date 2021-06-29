df24232bc23e372dcd6dc1947f978f7896fd8340
kain <kainpro@foxmail.com>
2021-06-29 21:32:08 +0800

    update

--- controller/main.go
+++ controller/main.go
@@ -2,7 +2,6 @@ package controller
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/kainonly/gin-planx/mvc"
 )
 
 type Main struct{}
@@ -12,7 +11,5 @@ func NewMain() *Main {
 }
 
 func (x *Main) Index(c *gin.Context) interface{} {
-	return mvc.Ok{
-		"msg": "hello",
-	}
+	return "hello"
 }
