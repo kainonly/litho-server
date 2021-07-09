3acaff59c4c07f9ba09c404b11aa6cdda2be4b87
kain <zhangtqx@vip.qq.com>
2021-07-09 18:40:09 +0800

    update

--- service/admin.go
+++ service/admin.go
@@ -1,9 +1,12 @@
 package service
 
 import (
+	"context"
+	jsoniter "github.com/json-iterator/go"
 	"github.com/kainonly/gin-helper/hash"
 	"github.com/kainonly/gin-helper/rbac"
 	"lab-api/model"
+	"strconv"
 )
 
 type Admin struct {
@@ -16,6 +19,7 @@ type Admin struct {
 func NewAdmin(dep *Dependent) *Admin {
 	return &Admin{
 		Dependent: dep,
+		key:       dep.Config.App.Key("system:admin"),
 	}
 }
 
@@ -37,4 +41,43 @@ func (x *Admin) Data(admin model.Admin) model.Admin {
 		Name:     admin.Name,
 		Status:   admin.Status,
 	}
+}
+
+func (x *Admin) Fetch(ctx context.Context, uid string) (result map[string]interface{}, err error) {
+	var exists int64
+	if exists, err = x.Redis.Exists(ctx, x.key).Result(); err != nil {
+		return
+	}
+	if exists == 0 {
+		var admins []model.AdminMix
+		if err = x.Db.Where("status = ?", true).Find(&admins).Error; err != nil {
+			return
+		}
+		lists := make(map[string]interface{})
+		for _, admin := range admins {
+			b, _ := jsoniter.Marshal(map[string]interface{}{
+				"id":         admin.ID,
+				"username":   admin.Username,
+				"password":   admin.Password,
+				"role":       admin.Role,
+				"resource":   admin.Resource,
+				"acl":        admin.Acl,
+				"permission": admin.Permission,
+			})
+			lists[strconv.FormatUint(admin.ID, 10)] = string(b)
+		}
+		x.Redis.HMSet(ctx, x.key, lists)
+	}
+	var b []byte
+	if b, err = x.Redis.HGet(ctx, x.key, uid).Bytes(); err != nil {
+		return
+	}
+	if b != nil {
+		jsoniter.Unmarshal(b, &result)
+	}
+	return
+}
+
+func (x *Admin) Clear(ctx context.Context) error {
+	return x.Redis.Del(ctx, x.key).Err()
 }
