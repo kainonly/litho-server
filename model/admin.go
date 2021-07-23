ef5da555892ca297e5d3f67cd9e9700ce05850c2
kain <kainpro@foxmail.com>
2021-07-23 17:43:49 +0800

    update

--- model/admin.go
+++ model/admin.go
@@ -4,9 +4,11 @@ import "time"
 
 type Admin struct {
 	ID         uint64    `json:"id"`
+	UUID       string    `gorm:"type:uuid;unique;default:gen_random_uuid()" json:"-"`
 	Username   string    `gorm:"type:varchar(20);not null;unique" json:"username"`
-	Password   string    `gorm:"type:varchar(255);not null" json:"password"`
-	Super      *bool     `gorm:"default:false"`
+	Password   string    `gorm:"type:varchar(255);not null" json:"-"`
+	Super      *bool     `gorm:"default:false" json:"-"`
+	Role       []string  `gorm:"type:varchar(20)[]" json:"-"`
 	Name       string    `gorm:"type:varchar(20)" json:"name"`
 	Email      string    `gorm:"type:varchar(255)" json:"email"`
 	Phone      string    `gorm:"type:varchar(20)" json:"phone"`
