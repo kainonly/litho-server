67d71e8d8e7567b6e3077b798e702594764a3832
kain <zhangtqx@vip.qq.com>
2021-08-08 19:04:27 +0800

    update

--- model/resource.go
+++ model/resource.go
@@ -3,10 +3,11 @@ package model
 type Resource struct {
 	Common
 
-	Parent   uint64 `json:"parent"`
-	Fragment string `gorm:"type:varchar(50);not null"`
-	Name     string `gorm:"type:varchar(20);not null"`
-	Nav      *bool  `gorm:"default:false"`
-	Router   *bool  `gorm:"default:false"`
-	Icon     string `gorm:"type:varchar(200)"`
+	Parent   uint64   `json:"parent"`
+	Fragment string   `gorm:"type:varchar(50);not null"`
+	Acl      []string `gorm:"type:varchar(50)[]"`
+	Name     string   `gorm:"type:varchar(20);not null"`
+	Nav      *bool    `gorm:"default:false"`
+	Router   *bool    `gorm:"default:false"`
+	Icon     string   `gorm:"type:varchar(200)"`
 }
