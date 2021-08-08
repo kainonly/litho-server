c837545bc7eb1137ed39ed30687272c037c7ab9f
kain <kainpro@foxmail.com>
2021-08-08 19:04:27 +0800

    update

--- model/admin.go
+++ model/admin.go
@@ -3,11 +3,12 @@ package model
 type Admin struct {
 	Common
 
-	Username string `gorm:"type:varchar(20);not null;unique" json:"username"`
-	Password string `gorm:"type:varchar(255);not null" json:"-"`
-	Super    *bool  `gorm:"default:false" json:"-"`
-	Name     string `gorm:"type:varchar(20)" json:"name"`
-	Email    string `gorm:"type:varchar(255)" json:"email"`
-	Phone    string `gorm:"type:varchar(20)" json:"phone"`
-	Avatar   string `gorm:"type:varchar(255)" json:"avatar"`
+	Username string   `gorm:"type:varchar(20);not null;unique" json:"username"`
+	Password string   `gorm:"type:varchar(255);not null" json:"-"`
+	Super    *bool    `gorm:"default:false" json:"-"`
+	Role     []string `gorm:"type:bigint[]" json:"-"`
+	Name     string   `gorm:"type:varchar(20)" json:"name"`
+	Email    string   `gorm:"type:varchar(255)" json:"email"`
+	Phone    string   `gorm:"type:varchar(20)" json:"phone"`
+	Avatar   string   `gorm:"type:varchar(255)" json:"avatar"`
 }
