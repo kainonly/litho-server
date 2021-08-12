3184e332cd7d20cfd6d91e26560668578291b63d
kain <zhangtqx@vip.qq.com>
2021-08-12 10:38:37 +0800

    update

--- model/resource.go
+++ model/resource.go
@@ -1,13 +1,16 @@
 package model
 
-type Resource struct {
-	Common
+import "time"
 
-	Parent   uint64   `json:"parent"`
-	Fragment string   `gorm:"type:varchar(50);not null"`
-	Acl      []string `gorm:"type:varchar(50)[]"`
-	Name     string   `gorm:"type:varchar(20);not null"`
-	Nav      *bool    `gorm:"default:false"`
-	Router   *bool    `gorm:"default:false"`
-	Icon     string   `gorm:"type:varchar(200)"`
+type Resource struct {
+	ID         uint64    `json:"id"`
+	Status     *bool     `gorm:"default:true" json:"status"`
+	CreateTime time.Time `gorm:"autoCreateTime" json:"create_time"`
+	UpdateTime time.Time `gorm:"autoUpdateTime" json:"update_time"`
+	Parent     uint64    `json:"parent"`
+	Fragment   string    `gorm:"type:varchar(50);not null"`
+	Name       string    `gorm:"type:varchar(20);not null"`
+	Nav        *bool     `gorm:"default:false"`
+	Router     *bool     `gorm:"default:false"`
+	Icon       string    `gorm:"type:varchar(200)"`
 }
