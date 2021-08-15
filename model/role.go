46c55b715f1f9dae9a4e045d8160a31b7b257fb2
kain <kainpro@foxmail.com>
2021-08-15 18:32:55 +0800

    update

--- model/role.go
+++ model/role.go
@@ -3,9 +3,10 @@ package model
 import "time"
 
 type Role struct {
-	ID         uint64    `json:"id"`
-	Status     *bool     `gorm:"default:true" json:"status"`
-	CreateTime time.Time `gorm:"autoCreateTime" json:"create_time"`
-	UpdateTime time.Time `gorm:"autoUpdateTime" json:"update_time"`
-	Resources  []string  ``
+	ID         uint64     `json:"id"`
+	Status     *bool      `gorm:"default:true" json:"status"`
+	CreateTime time.Time  `gorm:"autoCreateTime" json:"create_time"`
+	UpdateTime time.Time  `gorm:"autoUpdateTime" json:"update_time"`
+	Key        string     `gorm:""`
+	Resource   []Resource `gorm:"many2many:role_resource_rel;constraint:OnUpdate:CASCADE,OnDelete:CASCADE"`
 }
