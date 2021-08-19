be9d118277399ca218567bdb00471d4704be6d8d
kain <kainpro@foxmail.com>
2021-08-19 11:24:18 +0800

    update

--- model/role.go
+++ model/role.go
@@ -3,12 +3,12 @@ package model
 import "time"
 
 type Role struct {
-	ID          uint64     `json:"id"`
-	Status      *bool      `gorm:"default:true" json:"status"`
-	CreateTime  time.Time  `gorm:"autoCreateTime" json:"create_time"`
-	UpdateTime  time.Time  `gorm:"autoUpdateTime" json:"update_time"`
-	Key         string     `gorm:"type:varchar(20);not null;unique;comment:权限索引"`
-	Name        string     `gorm:"type:varchar(20);not null;comment:权限名称"`
-	Description string     `gorm:"type:text;comment:描述"`
-	Resources   []Resource `gorm:"many2many:role_resource_rel;constraint:OnDelete:CASCADE"`
+	ID          uint64       `json:"id"`
+	Status      *bool        `gorm:"default:true" json:"status"`
+	CreateTime  time.Time    `gorm:"autoCreateTime" json:"create_time"`
+	UpdateTime  time.Time    `gorm:"autoUpdateTime" json:"update_time"`
+	Name        string       `gorm:"type:varchar(20);not null;comment:名称"`
+	Description string       `gorm:"type:text;comment:描述"`
+	Resources   []Resource   `gorm:"many2many:role_resource_rel;constraint:OnDelete:CASCADE"`
+	Permission  []Permission `gorm:"many2many:role_permission_rel;constraint:OnDelete:CASCADE"`
 }
--- model/role_test.go
+++ model/role_test.go
@@ -8,14 +8,9 @@ func TestRole(t *testing.T) {
 	if err := db.AutoMigrate(&Role{}); err != nil {
 		t.Error(err)
 	}
-	var resources []Resource
-	if err := db.Select([]string{"id"}).Find(&resources).Error; err != nil {
-		t.Error(err)
-	}
 	if err := db.Create(&Role{
-		Key:       "*",
-		Name:      "超级管理员",
-		Resources: resources,
+		Name:        "超级管理员",
+		Description: "超级管理员权限不能删除，若不使用可以禁用该权限",
 	}).Error; err != nil {
 		t.Error(err)
 	}
