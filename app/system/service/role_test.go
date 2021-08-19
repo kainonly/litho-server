84c298deade640e619a6503965e4852c2b566a13
kain <zhangtqx@vip.qq.com>
2021-08-20 00:38:25 +0800

    update

--- app/system/service/role_test.go
+++ app/system/service/role_test.go
@@ -5,9 +5,14 @@ import (
 	"testing"
 )
 
-//func TestRole_GetFromCache(t *testing.T) {
-//	s.Role.GetFromCache()
-//}
+func TestRole_GetFromCache(t *testing.T) {
+	data, err := s.Role.GetFromCache(context.Background(), 2)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(data["resources"].([]interface{})[0])
+	t.Log(data["acls"].([]interface{})[0])
+}
 
 func TestRole_RefreshCache(t *testing.T) {
 	if err := s.Role.RefreshCache(context.Background()); err != nil {
