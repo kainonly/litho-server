730274509c9b91147a34ca66539bdbbe652ca38d
kain <kainpro@foxmail.com>
2021-09-17 19:24:08 +0800

    rebuild for nosql

--- bootstrap/bootstrap.go
+++ bootstrap/bootstrap.go
@@ -9,6 +9,7 @@ import (
 	"github.com/kainonly/go-bit/authx"
 	"github.com/kainonly/go-bit/cipher"
 	"github.com/kainonly/go-bit/cookie"
+	"github.com/kainonly/go-bit/crud"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
@@ -24,6 +25,7 @@ import (
 var Provides = fx.Provide(
 	LoadSettings,
 	InitializeDatabase,
+	crud.New,
 	InitializeRedis,
 	InitializeCookie,
 	InitializeAuthx,
