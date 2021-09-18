c836c908fff90e6b521d20aea0119573a7140bc5
kain <kainpro@foxmail.com>
2021-09-18 18:13:29 +0800

    rebuild

--- app/system/routes.go
+++ app/system/routes.go
@@ -2,59 +2,23 @@ package system
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/kainonly/go-bit/mvc"
 	"go.uber.org/fx"
-	"lab-api/app/system/admin"
-	"lab-api/app/system/devtools"
 	"lab-api/app/system/index"
 	"lab-api/app/system/resource"
-	"lab-api/app/system/role"
 	"lab-api/common"
-	"os"
 )
 
 var Options = fx.Options(
 	index.Provides,
 	resource.Provides,
-	role.Provides,
-	admin.Provides,
 	fx.Invoke(Routes),
 )
 
 type Inject struct {
 	common.App
-	DevTools *devtools.Controller
 	Index    *index.Controller
 	Resource *resource.Controller
-	Role     *role.Controller
-	Admin    *admin.Controller
 }
 
 func Routes(r *gin.Engine, i Inject) {
-	s := r.Group("system")
-	if os.Getenv("GIN_MODE") != "release" {
-		devtoolsRoute := s.Group("devtools")
-		devtoolsRoute.POST("setup", mvc.Returns(i.DevTools.Setup))
-		devtoolsRoute.POST("sync", mvc.Returns(i.DevTools.Sync))
-		devtoolsRoute.POST("migrate", mvc.Returns(i.DevTools.Migrate))
-	}
-	auth := authMiddleware(i.Authx.Make("system"), i.Cookie)
-	s.POST("login", mvc.Returns(i.Index.Login))
-	s.POST("verify", mvc.Returns(i.Index.Verify))
-	s.POST("code", auth, mvc.Returns(i.Index.Code))
-	s.POST("refresh", auth, mvc.Returns(i.Index.RefreshToken))
-	s.POST("logout", auth, mvc.Returns(i.Index.Logout))
-	s.POST("resource", auth, mvc.Returns(i.Index.Resource))
-	resourceRoute := s.Group("resource", auth)
-	{
-		mvc.Crud(resourceRoute, i.Resource)
-	}
-	roleRoute := s.Group("role", auth)
-	{
-		mvc.Crud(roleRoute, i.Role)
-	}
-	adminRoute := s.Group("admin", auth)
-	{
-		mvc.Crud(adminRoute, i.Admin)
-	}
 }
--- go.mod
+++ go.mod
@@ -3,21 +3,16 @@ module lab-api
 go 1.15
 
 require (
-	github.com/alexedwards/argon2id v0.0.0-20210511081203-7d35d68092b8
 	github.com/gin-contrib/cors v1.3.1
 	github.com/gin-gonic/gin v1.7.3
 	github.com/go-redis/redis/v8 v8.11.3
-	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/json-iterator/go v1.1.11
 	github.com/kainonly/go-bit v1.0.1-beta.0.20210813060118-d167d0efebac
-	github.com/lib/pq v1.6.0 // indirect
-	github.com/thoas/go-funk v0.9.1
-	go.mongodb.org/mongo-driver v1.7.2 // indirect
+	go.mongodb.org/mongo-driver v1.7.2
 	go.uber.org/fx v1.14.2
 	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
-	gorm.io/driver/postgres v1.1.0
 	gorm.io/gorm v1.21.12
 )
 
