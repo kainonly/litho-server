782f3c6052f670f4e224cbaad8734c0d31493c05
kain <zhangtqx@vip.qq.com>
2021-09-24 17:18:52 +0800

    rebuild

--- api/routes.go
+++ api/routes.go
@@ -2,21 +2,22 @@ package api
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/kainonly/go-bit/authx"
+	"github.com/kainonly/go-bit/dsapi"
 	"github.com/kainonly/go-bit/mvc"
 	"go.uber.org/fx"
 	"lab-api/api/index"
-	"lab-api/common"
 )
 
-var Options = fx.Options(index.Provides, fx.Invoke(Routes))
-
-type Inject struct {
-	common.App
-
-	Index *index.Controller
-}
-
-func Routes(r *gin.Engine, i Inject) {
-	mvc.New(r, i.Index)
-	mvc.New(r, i.API, mvc.SetPath("dsapi"))
-}
+var Options = fx.Options(
+	index.Provides,
+	fx.Invoke(func(
+		route *gin.Engine,
+		api *dsapi.API,
+		authx *authx.Authx,
+		index *index.Controller,
+	) {
+		mvc.New(route, index)
+		mvc.New(route, api, mvc.SetPath("dsapi"))
+	}),
+)
