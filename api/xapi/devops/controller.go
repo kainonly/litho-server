7d4ca5592479219ef7514dff8aaf01944257dfc7
kain <zhangtqx@vip.qq.com>
2021-10-08 17:16:00 +0800

    update

--- api/xapi/devops/controller.go
+++ api/xapi/devops/controller.go
@@ -1,6 +1,8 @@
 package devops
 
 import (
+	"github.com/gin-gonic/gin"
+	"github.com/weplanx/support/basic"
 	"laboratory/common"
 )
 
@@ -13,31 +15,12 @@ type InjectController struct {
 	Service *Service
 }
 
-//func (x *Controller) Setup(c *gin.Context) interface{} {
-//	tx := x.Db.WithContext(c)
-//	if err := basic.GenerateSchema(tx); err != nil {
-//		return err
-//	}
-//	if err := basic.GeneratePage(tx); err != nil {
-//		return err
-//	}
-//	if err, ok := x.Sync(c).(error); ok {
-//		return err
-//	}
-//	return "ok"
-//}
-//
-//func (x *Controller) Sync(c *gin.Context) interface{} {
-//	buf, err := basic.GenerateModel(x.Db.WithContext(c))
-//	if err != nil {
-//		return err
-//	}
-//	b, err := format.Source(buf.Bytes())
-//	if err != nil {
-//		return err
-//	}
-//	if err = ioutil.WriteFile("./model/model_gen.go", b, fs.ModePerm); err != nil {
-//		return err
-//	}
-//	return "ok"
-//}
+func (x *Controller) Setup(c *gin.Context) interface{} {
+	if err := basic.GenerateSchema(c, x.Db); err != nil {
+		return err
+	}
+	if err := basic.GeneratePage(c, x.Db); err != nil {
+		return err
+	}
+	return "ok"
+}
