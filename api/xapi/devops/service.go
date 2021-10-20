6096ab9c0fa893ff7243fa17e30d9ad74c4218d5
kain <kainpro@foxmail.com>
2021-10-20 18:36:26 +0800

    update

--- api/xapi/devops/service.go
+++ api/xapi/devops/service.go
@@ -1,6 +1,8 @@
 package devops
 
 import (
+	"context"
+	"go.mongodb.org/mongo-driver/bson"
 	"laboratory/common"
 )
 
@@ -10,4 +12,27 @@ type Service struct {
 
 type InjectService struct {
 	common.App
+}
+
+func (x *Service) InitData(ctx context.Context) (err error) {
+	if _, err = x.Db.Collection("role").InsertOne(ctx, bson.M{
+		"key":         "*",
+		"name":        "超级管理员",
+		"description": "",
+		"pages":       bson.D{},
+	}); err != nil {
+		return
+	}
+	if _, err = x.Db.Collection("admin").InsertOne(ctx, bson.M{
+		"username": "admin",
+		"password": "",
+		"roles":    bson.A{"*"},
+		"name":     "",
+		"email":    "",
+		"phone":    "",
+		"avatar":   "",
+	}); err != nil {
+		return
+	}
+	return
 }
