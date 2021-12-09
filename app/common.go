78ab913b0ced11ba15e35cfac35d139c94356d60
kain <zhangtqx@vip.qq.com>
2021-12-09 16:53:20 +0800

    update

--- app/common.go
+++ app/common.go
@@ -1,9 +1,8 @@
 package app
 
 import (
-	"api/app/admin"
 	"api/app/index"
-	"api/app/page"
+	"api/app/pages"
 	"github.com/gofiber/fiber/v2"
 	"github.com/weplanx/go/api"
 	"github.com/weplanx/go/passport"
@@ -13,18 +12,19 @@ import (
 
 var Options = fx.Options(
 	index.Provides,
-	admin.Provides,
-	page.Provides,
+	pages.Provides,
 	fx.Invoke(func(
 		app *fiber.App,
 		api *api.API,
 		passport *passport.Passport,
 		index *index.Controller,
+		pages *pages.Controller,
 	) {
-		//auth := AuthGuard(passport)
-
 		app.Get("/", route.Returns(Home))
+
+		//auth := AuthGuard(passport)
 		route.Auto(app, index)
+		route.Auto(app, pages, route.SetPath("/pages"))
 		route.Auto(app, api, route.SetPath(":collection"))
 		//route.Auto(app, index, route.SetMiddleware(auth, "Code", "RefreshToken", "Logout", "Pages"))
 		//route.Auto(app, api, route.SetPath(":collection"), route.SetMiddleware(auth))
