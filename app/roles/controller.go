26f903dd774908c8f655bfc24e3d272f842778d0
kain <kainpro@foxmail.com>
2021-12-21 19:27:13 +0800

    update

--- app/roles/controller.go
+++ app/roles/controller.go
@@ -2,7 +2,10 @@ package roles
 
 import (
 	"api/common"
+	"api/model"
+	"github.com/gofiber/fiber/v2"
 	"github.com/weplanx/go/api"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Controller struct {
@@ -14,4 +17,26 @@ type InjectController struct {
 	common.Inject
 	APIs    *api.API
 	Service *Service
+}
+
+func (x *Controller) Create(c *fiber.Ctx) interface{} {
+	var body model.Role
+	if err := c.BodyParser(&body); err != nil {
+		return err
+	}
+	var parent primitive.ObjectID
+	if body.Parent != "root" {
+		var err error
+		if parent, err = primitive.ObjectIDFromHex(body.Parent.(string)); err != nil {
+			return err
+		}
+	}
+	result, err := x.APIs.Create(
+		c.UserContext(),
+		model.NewRole(body.Key, body.Name).SetParent(parent),
+	)
+	if err != nil {
+		return err
+	}
+	return result
 }
