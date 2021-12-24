8aac3ca3c5aad642cc38a60860bbb0dd00f5d7c7
kain <zhangtqx@vip.qq.com>
2021-12-24 11:31:36 +0800

    update

--- app/common.go
+++ app/common.go
@@ -5,19 +5,23 @@ import (
 	"api/common"
 	"github.com/gin-gonic/gin"
 	"github.com/google/wire"
+	"github.com/weplanx/go/api"
 	"github.com/weplanx/go/route"
 )
 
 var Provides = wire.NewSet(
 	index.Provides,
+	api.Provides,
 	New,
 )
 
 func New(
 	values *common.Values,
 	index *index.Controller,
+	api *api.Controller,
 ) *gin.Engine {
 	r := middleware(gin.New(), values)
 	r.GET("/", route.Use(index.Index))
+	api.Auto(r)
 	return r
 }
