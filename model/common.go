126ac2b75f693804604aee4c18a2c0111da3bb27
kain <kainpro@foxmail.com>
2022-01-16 13:10:29 +0800

    update

--- model/common.go
+++ model/common.go
@@ -3,3 +3,11 @@ package model
 func Bool(v bool) *bool {
 	return &v
 }
+
+type Value struct {
+	// 名称
+	Label string `bson:"label" json:"label"`
+
+	// 数值
+	Value interface{} `bson:"value" json:"value"`
+}
