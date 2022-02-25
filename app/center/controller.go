978e44ac80b97a6ae14b2890a7214aa1d9148fea
kain <kainpro@foxmail.com>
2022-02-25 18:19:40 +0800

    update

--- app/center/controller.go
+++ app/center/controller.go
@@ -3,6 +3,7 @@ package center
 import (
 	"api/app/users"
 	"api/common"
+	"api/model"
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v4"
 	"go.mongodb.org/mongo-driver/bson"
@@ -22,14 +23,46 @@ func (x *Controller) GetUserInfo(c *gin.Context) interface{} {
 	data, err := x.Users.FindOneById(ctx,
 		claimsContext["uid"].(string),
 		options.FindOne().SetProjection(bson.M{
+			"_id":      0,
 			"password": 0,
 			"roles":    0,
 			"pages":    0,
 			"readonly": 0,
+			"labels":   0,
+			"status":   0,
 		}),
 	)
 	if err != nil {
 		return err
 	}
 	return data
+}
+
+type SetUserInfoDto struct {
+	Name         string        `json:"name"`
+	Avatar       string        `json:"avatar"`
+	Region       string        `json:"region"`
+	City         string        `json:"city"`
+	Address      string        `json:"address"`
+	Introduction string        `json:"introduction"`
+	Phone        []model.Phone `json:"phone"`
+	Email        []string      `json:"email"`
+}
+
+// SetUserInfo 更新用户信息
+func (x *Controller) SetUserInfo(c *gin.Context) interface{} {
+	var body SetUserInfoDto
+	if err := c.ShouldBindJSON(&body); err != nil {
+		return err
+	}
+	value, _ := c.Get(common.TokenClaimsKey)
+	claimsContext := value.(jwt.MapClaims)["context"].(map[string]interface{})
+	ctx := c.Request.Context()
+	if err := x.Users.UpdateOneById(ctx,
+		claimsContext["uid"].(string),
+		bson.M{"$set": body},
+	); err != nil {
+		return err
+	}
+	return nil
 }
