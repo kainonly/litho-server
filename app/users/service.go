e70f50efbb7d584ab2f320fe8b7dadf249a04ed2
kain <zhangtqx@vip.qq.com>
2022-04-11 15:46:41 +0800

    更新：适配

--- app/users/service.go
+++ app/users/service.go
@@ -13,27 +13,6 @@ type Service struct {
 	*common.Inject
 }
 
-func (x *Service) HasUsername(ctx context.Context, username string) (code string, err error) {
-	var count int64
-	if count, err = x.Db.Collection("users").CountDocuments(ctx, bson.M{
-		"username": username,
-	}); err != nil {
-		return
-	}
-	if count != 0 {
-		return "duplicated", nil
-	}
-	return "", err
-}
-
-func (x *Service) FindLabels(ctx context.Context) (values []interface{}, err error) {
-	if values, err = x.Db.Collection("users").
-		Distinct(ctx, "labels", bson.M{"status": true}); err != nil {
-		return
-	}
-	return
-}
-
 func (x *Service) FindOneByUsername(ctx context.Context, username string) (data model.User, err error) {
 	if err = x.Db.Collection("users").
 		FindOne(ctx, bson.M{
