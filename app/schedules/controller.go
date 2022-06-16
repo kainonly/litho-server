1072a7bd853ebe382aa39013315343cc42c5ff9f
kain <kainpro@foxmail.com>
2022-06-16 16:10:21 +0800

    更新：通用查询路由前缀 api/ 变更为 dsl/

--- app/schedules/controller.go
+++ app/schedules/controller.go
@@ -1,5 +1,15 @@
 package schedules
 
+import "github.com/gin-gonic/gin"
+
 type Controller struct {
 	Service *Service
+}
+
+func (x *Controller) GetKeys(c *gin.Context) interface{} {
+	keys, err := x.Service.GetKeys()
+	if err != nil {
+		return err
+	}
+	return keys
 }
--- app/schedules/service.go
+++ app/schedules/service.go
@@ -9,3 +9,8 @@ type Service struct {
 	*common.Inject
 	Client *client.Schedule
 }
+
+// GetKeys 获取调度服务已存在的标识
+func (x *Service) GetKeys() (keys []string, err error) {
+	return x.Client.List()
+}
