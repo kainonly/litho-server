50c00e99167fedd5694f09506b8485aea03ea136
kain <zhangtqx@vip.qq.com>
2022-06-22 14:04:44 +0800

    更新：测试

---
+++ app/schedules/common_test.go
@@ -0,0 +1,18 @@
+package schedules
+
+import (
+	"api/common"
+	"api/test"
+	"os"
+	"testing"
+)
+
+var i *common.Inject
+var service *Service
+
+func TestMain(m *testing.M) {
+	os.Chdir("../../")
+	i, _ = test.Bed()
+	service = &Service{Inject: i}
+	os.Exit(m.Run())
+}
