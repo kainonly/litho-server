7e79580d7fc7e47a709fd53130fc961afc7817d9
kain <zhangtqx@vip.qq.com>
2022-07-29 17:07:52 +0800

    update: 工具库分离

--- api/index/service.go
+++ api/index/service.go
@@ -12,6 +12,8 @@ import (
 	"github.com/weplanx/server/api/sessions"
 	"github.com/weplanx/server/api/users"
 	"github.com/weplanx/server/common"
+	"github.com/weplanx/server/common/captcha"
+	"github.com/weplanx/server/common/locker"
 	"github.com/weplanx/server/common/passlib"
 	"github.com/weplanx/server/model"
 	"go.mongodb.org/mongo-driver/bson"
@@ -27,6 +29,9 @@ type Service struct {
 	UsersService       *users.Service
 	RolesService       *roles.Service
 	DepartmentsService *departments.Service
+
+	Captcha *captcha.Captcha
+	Locker  *locker.Locker
 }
 
 // Login 登录
@@ -39,7 +44,7 @@ func (x *Service) Login(ctx context.Context, identity string, password string) (
 
 	// 锁定上限验证
 	var maxFailures bool
-	if maxFailures, err = x.VerifyLocker(ctx, uid, x.Values.GetLoginFailures()); err != nil {
+	if maxFailures, err = x.Locker.Verify(ctx, uid, x.Values.GetLoginFailures()); err != nil {
 		return
 	}
 	if maxFailures {
@@ -50,7 +55,7 @@ func (x *Service) Login(ctx context.Context, identity string, password string) (
 	// 验证密码正确性
 	if err = passlib.Verify(password, user.Password); err != nil {
 		// 锁定更新
-		if err = x.UpdateLocker(ctx, uid, x.Values.GetLoginTTL()); err != nil {
+		if err = x.Locker.Update(ctx, uid, x.Values.GetLoginTTL()); err != nil {
 			return
 		}
 		if err == passlib.ErrNotMatch {
@@ -69,7 +74,7 @@ func (x *Service) Login(ctx context.Context, identity string, password string) (
 
 // LoginSession 建立登录会话，移除锁定
 func (x *Service) LoginSession(ctx context.Context, uid string, jti string) (err error) {
-	if err = x.DeleteLocker(ctx, jti); err != nil {
+	if err = x.Locker.Delete(ctx, jti); err != nil {
 		return
 	}
 	if err = x.SessionService.Set(ctx, uid, jti); err != nil {
@@ -110,6 +115,17 @@ func (x *Service) GetNavs(ctx context.Context, uid string) (_ []pages.Nav, err e
 	//}
 
 	return x.PagesService.FindNavs(ctx)
+}
+
+// GetRefreshCode 获取刷新令牌验证码
+func (x *Service) GetRefreshCode(ctx context.Context, uid string) (code string, err error) {
+	if code, err = gonanoid.Nanoid(); err != nil {
+		return
+	}
+	if err = x.Captcha.Create(ctx, uid, code, 15*time.Second); err != nil {
+		return
+	}
+	return
 }
 
 // GetOptions 返回通用配置
@@ -192,117 +208,4 @@ func (x *Service) UnsetUser(ctx context.Context, id string, mate string) (interf
 		"$unset": bson.M{mate: ""},
 	}
 	return x.UsersService.UpdateOneById(ctx, oid, update)
-}
-
-// Captcha 验证命名
-func (x *Service) Captcha(name string) string {
-	return x.Values.Key("captcha", name)
-}
-
-// CreateCaptcha 创建验证码
-func (x *Service) CreateCaptcha(ctx context.Context, name string, code string, ttl time.Duration) error {
-	return x.Redis.
-		Set(ctx, x.Captcha(name), code, ttl).
-		Err()
-}
-
-// ExistsCaptcha 存在验证码
-func (x *Service) ExistsCaptcha(ctx context.Context, name string) (_ bool, err error) {
-	var exists int64
-	if exists, err = x.Redis.
-		Exists(ctx, x.Captcha(name)).
-		Result(); err != nil {
-
-	}
-	return exists != 0, nil
-}
-
-var (
-	ErrCaptchaNotExists    = errors.NewPublic("验证码不存在")
-	ErrCaptchaInconsistent = errors.NewPublic("无效的验证码")
-)
-
-// VerifyCaptcha 校验验证码
-func (x *Service) VerifyCaptcha(ctx context.Context, name string, code string) (err error) {
-	var exists bool
-	if exists, err = x.ExistsCaptcha(ctx, name); err != nil {
-		return
-	}
-	if !exists {
-		return ErrCaptchaNotExists
-	}
-
-	var value string
-	if value, err = x.Redis.
-		Get(ctx, x.Captcha(name)).
-		Result(); err != nil {
-		return
-	}
-	if value != code {
-		return ErrCaptchaInconsistent
-	}
-
-	return
-}
-
-// DeleteCaptcha 移除验证码
-func (x *Service) DeleteCaptcha(ctx context.Context, name string) error {
-	return x.Redis.Del(ctx, x.Captcha(name)).Err()
-}
-
-// Locker 锁定命名
-func (x *Service) Locker(name string) string {
-	return x.Values.Key("locker", name)
-}
-
-// UpdateLocker 更新锁定
-func (x *Service) UpdateLocker(ctx context.Context, name string, ttl time.Duration) (err error) {
-	var exists int64
-	if exists, err = x.Redis.
-		Exists(ctx, x.Locker(name)).
-		Result(); err != nil {
-		return
-	}
-
-	if exists == 0 {
-		if err = x.Redis.
-			Set(ctx, x.Locker(name), 1, ttl).
-			Err(); err != nil {
-			return
-		}
-	} else {
-		if err = x.Redis.
-			Incr(ctx, x.Locker(name)).
-			Err(); err != nil {
-			return
-		}
-	}
-	return
-}
-
-// VerifyLocker 校验锁定，True 为锁定
-func (x *Service) VerifyLocker(ctx context.Context, name string, n int64) (result bool, err error) {
-	var exists int64
-	if exists, err = x.Redis.
-		Exists(ctx, x.Locker(name)).
-		Result(); err != nil {
-		return
-	}
-	if exists == 0 {
-		return
-	}
-
-	var count int64
-	if count, err = x.Redis.
-		Get(ctx, x.Locker(name)).
-		Int64(); err != nil {
-		return
-	}
-
-	return count >= n, nil
-}
-
-// DeleteLocker 移除锁定
-func (x *Service) DeleteLocker(ctx context.Context, name string) error {
-	return x.Redis.Del(ctx, x.Locker(name)).Err()
 }
