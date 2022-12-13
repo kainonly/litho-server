39a786519840b915b6546fad339e01955299d5b7
kain <zhangtqx@vip.qq.com>
2022-12-13 16:02:48 +0800

    UPDATE: Index API

--- api/index/service_test.go
+++ api/index/service_test.go
@@ -1,14 +1,8 @@
 package index_test
 
-import (
-	"context"
-	"github.com/stretchr/testify/assert"
-	"testing"
-)
-
-func TestService_Login(t *testing.T) {
-	ctx := context.TODO()
-	ts, err := x.IndexService.Login(ctx, "weplanx", "pass@VAN1234")
-	assert.Nil(t, err)
-	t.Log(ts)
-}
+//func TestService_Login(t *testing.T) {
+//	ctx := context.TODO()
+//	ts, err := x.IndexService.Login(ctx, "weplanx", "pass@VAN1234")
+//	assert.Nil(t, err)
+//	t.Log(ts)
+//}
