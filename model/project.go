713a215d897c84cbe2a0465b452fb734ed106ed2
kain <kainpro@foxmail.com>
2022-12-17 23:13:44 +0800

    UPDATE

--- model/project.go
+++ model/project.go
@@ -6,25 +6,25 @@ import (
 )
 
 type Project struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
-	Name        string             `bson:"name" json:"name"`
-	Namespace   string             `bson:"namespace" json:"namespace"`
-	Secret      string             `bson:"secret" json:"secret"`
-	Entry       []string           `bson:"entry" json:"entry"`
-	Expire      int64              `bson:"expire" json:"expire"`
-	Status      bool               `bson:"status" json:"status"`
-	CreatedTime time.Time          `json:"created_time"`
-	UpdatedTime time.Time          `json:"updated_time"`
+	ID         primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Name       string             `bson:"name" json:"name"`
+	Namespace  string             `bson:"namespace" json:"namespace"`
+	Secret     string             `bson:"secret" json:"secret"`
+	Entry      []string           `bson:"entry" json:"entry"`
+	Expire     int64              `bson:"expire" json:"expire"`
+	Status     bool               `bson:"status" json:"status"`
+	CreateTime time.Time          `bson:"create_time" json:"create_time"`
+	UpdateTime time.Time          `bson:"update_time" json:"update_time"`
 }
 
 func NewProject(name string, namespace string) *Project {
 	return &Project{
-		Name:        name,
-		Namespace:   namespace,
-		Entry:       []string{},
-		Expire:      0,
-		Status:      true,
-		CreatedTime: time.Now(),
-		UpdatedTime: time.Now(),
+		Name:       name,
+		Namespace:  namespace,
+		Entry:      []string{},
+		Expire:     0,
+		Status:     true,
+		CreateTime: time.Now(),
+		UpdateTime: time.Now(),
 	}
 }
