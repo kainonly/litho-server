4e2e579bfecff01ec6f8cd5fa312e05372b4dc57
kain <kainpro@foxmail.com>
2023-07-25 23:37:46 +0800

    update: index api

--- api/index/controller.go
+++ api/index/controller.go
@@ -2,17 +2,23 @@ package index
 
 import (
 	"context"
+	"fmt"
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/common/utils"
 	"github.com/cloudwego/hertz/pkg/protocol"
+	"github.com/huandu/xstrings"
 	"github.com/weplanx/go/csrf"
+	"github.com/weplanx/go/passlib"
 	"github.com/weplanx/server/common"
+	"go.mongodb.org/mongo-driver/bson"
 	"net/http"
+	"reflect"
 	"time"
 )
 
 type Controller struct {
 	IndexService *Service
+	V            *common.Values
 	Csrf         *csrf.Csrf
 }
 
@@ -119,4 +125,115 @@ func (x *Controller) Logout(ctx context.Context, c *app.RequestContext) {
 		"code":    0,
 		"message": "ok",
 	})
+}
+
+func (x *Controller) GetUser(ctx context.Context, c *app.RequestContext) {
+	claims := common.GetClaims(c)
+	data, err := x.IndexService.GetUser(ctx, claims.UserId)
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, data)
+}
+
+type SetUserDto struct {
+	Set         string `json:"$set,required" vd:"in($, 'email', 'name', 'avatar', 'password', 'backup_email')"`
+	Email       string `json:"email,omitempty" vd:"(Set)$!='Email' || email($);msg:'must be email'"`
+	BackupEmail string `json:"backup_email,omitempty" vd:"(Set)$!='BackupEmail' || email($);msg:'must be email'"`
+	Name        string `json:"name,omitempty"`
+	Avatar      string `json:"avatar,omitempty"`
+	Password    string `json:"password,omitempty" vd:"(Set)$!='Password' || len($)>8;msg:'must be greater than 8 characters'"`
+}
+
+func (x *Controller) SetUser(ctx context.Context, c *app.RequestContext) {
+	var dto SetUserDto
+	if err := c.BindAndValidate(&dto); err != nil {
+		c.Error(err)
+		return
+	}
+	data := make(map[string]interface{})
+	path := xstrings.ToCamelCase(dto.Set)
+	value := reflect.ValueOf(dto).FieldByName(path).Interface()
+	if dto.Set == "password" {
+		data[dto.Set], _ = passlib.Hash(value.(string))
+	} else {
+		data[dto.Set] = value
+	}
+
+	claims := common.GetClaims(c)
+	_, err := x.IndexService.SetUser(ctx, claims.UserId, bson.M{
+		"$set": data,
+	})
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, utils.H{
+		"code":    0,
+		"message": "ok",
+	})
+}
+
+type UnsetUserDto struct {
+	Key string `path:"key,required" vd:"in($, 'lark')"`
+}
+
+func (x *Controller) UnsetUser(ctx context.Context, c *app.RequestContext) {
+	var dto UnsetUserDto
+	if err := c.BindAndValidate(&dto); err != nil {
+		c.Error(err)
+		return
+	}
+
+	claims := common.GetClaims(c)
+	_, err := x.IndexService.SetUser(ctx, claims.UserId, bson.M{
+		"$unset": bson.M{dto.Key: 1},
+	})
+	if err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(http.StatusOK, utils.H{
+		"code":    0,
+		"message": "ok",
+	})
+}
+
+type OptionsDto struct {
+	Type string `query:"type"`
+}
+
+// Options 返回通用配置
+func (x *Controller) Options(ctx context.Context, c *app.RequestContext) {
+	var dto OptionsDto
+	if err := c.BindAndValidate(&dto); err != nil {
+		c.Error(err)
+		return
+	}
+	switch dto.Type {
+	case "upload":
+		switch x.V.Cloud {
+		case "tencent":
+			c.JSON(http.StatusOK, utils.H{
+				"type": "cos",
+				"url": fmt.Sprintf(`https://%s.cos.%s.myqcloud.com`,
+					x.V.TencentCosBucket, x.V.TencentCosRegion,
+				),
+				"limit": x.V.TencentCosLimit,
+			})
+			return
+		}
+	case "collaboration":
+		c.JSON(http.StatusOK, utils.H{
+			"url":      "https://open.larksuite.com/open-apis/authen/v1/index",
+			"redirect": x.V.RedirectUrl,
+			"app_id":   x.V.LarkAppId,
+		})
+		return
+	}
+	return
 }
