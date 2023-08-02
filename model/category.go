3546200f7aba6656e26bc2d036b427b12708b741
kain <zhangtqx@vip.qq.com>
2023-08-03 01:13:26 +0800

    update: categories

--- model/category.go
+++ model/category.go
@@ -7,7 +7,9 @@ import (
 
 type Category struct {
 	ID         primitive.ObjectID `bson:"_id,omitempty" json:"_id"`
+	Type       string             `bson:"type" json:"type"`
 	Name       string             `bson:"name" json:"name"`
+	Sort       int64              `bson:"sort" json:"sort"`
 	CreateTime time.Time          `bson:"create_time" json:"create_time"`
 	UpdateTime time.Time          `bson:"update_time" json:"update_time"`
 }
