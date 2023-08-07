bf3f8eb6bd923f6fa2f953744e844f8539c999d5
kain <zhangtqx@vip.qq.com>
2023-08-07 11:17:55 +0800

    update: user detail

--- model/logset_logined.go
+++ model/logset_logined.go
@@ -8,15 +8,15 @@ import (
 type LogsetLogined struct {
 	Timestamp time.Time             `bson:"timestamp" json:"timestamp"`
 	Metadata  LogsetLoginedMetadata `bson:"metadata" json:"metadata"`
-	UserAgent string                `bson:"user_agent"`
-	Detail    interface{}           `bson:"detail"`
+	UserAgent string                `bson:"user_agent" json:"user_agent"`
+	Detail    interface{}           `bson:"detail" json:"detail"`
 }
 
 type LogsetLoginedMetadata struct {
-	UserID   primitive.ObjectID `bson:"user_id"`
-	ClientIP string             `bson:"client_ip"`
-	Version  string             `bson:"version"`
-	Source   string             `bson:"source" json:"source"`
+	UserID   primitive.ObjectID `bson:"user_id" json:"-"`
+	ClientIP string             `bson:"client_ip" json:"client_ip"`
+	Version  string             `bson:"version" json:"version"`
+	Source   string             `bson:"source" json:"source" json:"source"`
 }
 
 func (x *LogsetLogined) SetDetail(v interface{}) {
