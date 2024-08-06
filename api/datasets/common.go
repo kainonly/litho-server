af153aa1a67f1e87454e39f3d36e01cf55ad1245
kain <zhangtqx@vip.qq.com>
2024-08-06 22:22:03 +0800

    update: adjust the structure

--- api/datasets/common.go
+++ api/datasets/common.go
@@ -2,6 +2,8 @@ package datasets
 
 import (
 	"github.com/google/wire"
+	"github.com/weplanx/go/values"
+	"github.com/weplanx/server/common"
 )
 
 var Provides = wire.NewSet(
@@ -9,4 +11,24 @@ var Provides = wire.NewSet(
 	wire.Struct(new(Service), "*"),
 )
 
+type Controller struct {
+	DatasetsX *Service
+}
+
+type Service struct {
+	*common.Inject
+
+	Values *values.Service
+}
+
 type M map[string]interface{}
+
+type Dataset struct {
+	Name       string   `bson:"name" json:"name"`
+	Type       string   `bson:"type" json:"type"`
+	Keys       []string `bson:"-" json:"keys"`
+	Sensitives []string `bson:"-" json:"sensitives"`
+	Status     bool     `bson:"-" json:"status"`
+	Event      bool     `bson:"-" json:"event"`
+	Options    M        `bson:"options" json:"options"`
+}
