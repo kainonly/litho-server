879b5d9c722a7a689af32910aadff1ed83002a3c
kain <kainpro@foxmail.com>
2025-09-27 21:02:37 +0800

    update: simplify

--- api/index/ping.go
+++ api/index/ping.go
@@ -2,16 +2,16 @@ package index
 
 import (
 	"context"
-	"github.com/cloudwego/hertz/pkg/app"
 	"os"
 	"time"
+
+	"github.com/cloudwego/hertz/pkg/app"
 )
 
 func (x *Controller) Ping(_ context.Context, c *app.RequestContext) {
 	data := M{
 		"hostname": os.Getenv("HOSTNAME"),
-		"endpoint": "hertz-server",
-		"ip":       string(c.GetHeader(x.V.Ip)),
+		"endpoint": "litho-server",
 		"now":      time.Now(),
 	}
 	c.JSON(200, data)
--- common/values.go
+++ common/values.go
@@ -1,43 +1,13 @@
 package common
 
-import "fmt"
-
 type Values struct {
-	Mode      string   `yaml:"mode"`
-	Address   string   `yaml:"address"`
-	Ip        string   `yaml:"ip"`
-	Namespace string   `yaml:"namespace"`
-	Key       string   `yaml:"key"`
-	Domain    string   `yaml:"domain"`
-	Database  Database `yaml:"database"`
-	Cos       Cos      `yaml:"cos"`
+	Address  string   `yaml:"address"`
+	Key      string   `yaml:"key"`
+	Domain   string   `yaml:"domain"`
+	Database Database `yaml:"database"`
 }
 
 type Database struct {
-	Url   string `yaml:"url"`
-	Name  string `yaml:"name"`
-	Redis string `yaml:"redis"`
-	Debug bool   `yaml:"debug"`
-}
-
-type Cos struct {
-	Bucket    string `yaml:"bucket"`
-	Region    string `yaml:"region"`
-	SecretId  string `yaml:"secret_id"`
-	SecretKey string `yaml:"secret_key"`
-}
-
-func (x Values) IsRelease() bool {
-	return x.Mode == "release"
-}
-
-func (x Values) IsSqlDebug() bool {
-	return x.Database.Debug
-}
-
-func (x Values) LogName(key string) string {
-	if x.IsRelease() {
-		return fmt.Sprintf(`%s_logs`, key)
-	}
-	return fmt.Sprintf(`%s_logs_dev`, key)
+	Url  string `yaml:"url"`
+	Name string `yaml:"name"`
 }
