807dcdc2b3c6b7df7c8250863a50335571b54ab3
kain <kainpro@foxmail.com>
2025-10-12 10:37:36 +0800

    update: simplify

---
+++ api/teams/delete.go
@@ -0,0 +1,30 @@
+package teams
+
+import (
+	"context"
+	"server/common"
+	"server/model"
+
+	"github.com/cloudwego/hertz/pkg/app"
+	"github.com/kainonly/go/help"
+)
+
+func (x *Controller) Delete(ctx context.Context, c *app.RequestContext) {
+	var dto common.DeleteDto
+	if err := c.BindAndValidate(&dto); err != nil {
+		c.Error(err)
+		return
+	}
+
+	user := common.GetIAM(c)
+	if err := x.TeamsX.Delete(ctx, user, dto); err != nil {
+		c.Error(err)
+		return
+	}
+
+	c.JSON(200, help.Ok())
+}
+
+func (x *Service) Delete(ctx context.Context, user *common.IAMUser, dto common.DeleteDto) (err error) {
+	return x.Db.WithContext(ctx).Delete(model.Team{}, dto.IDs).Error
+}
